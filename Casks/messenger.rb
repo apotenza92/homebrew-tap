24dbcf0d1988026837723a43e01b7c026f17ad71
Alex <apotenza92@gmail.com>
2026-01-01 13:28:15 +1100

    Update messenger cask with real SHA256 hashes for v0.1.9

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -2,18 +2,18 @@ cask "messenger" do
   version "0.1.9"
 
   on_arm do
-    sha256 "REPLACE_WITH_ARM64_SHA256"
-    url "https://github.com/apotenza92/FacebookMessengerDesktop/releases/download/v#{version}/Messenger-#{version}-macos-arm64.zip"
+    sha256 "e7dfca928f6ea16580797d0880908e205a531a75fe2902e23ac73923a94dd84d"
+    url "https://github.com/apotenza92/facebook-messenger-desktop/releases/download/v#{version}/Messenger-#{version}-macos-arm64.zip"
   end
 
   on_intel do
-    sha256 "REPLACE_WITH_X64_SHA256"
-    url "https://github.com/apotenza92/FacebookMessengerDesktop/releases/download/v#{version}/Messenger-#{version}-macos-x64.zip"
+    sha256 "71894cc106eec5a0648a44d32b80877a9604108b0f0d8a5ea2c8e027c0234703"
+    url "https://github.com/apotenza92/facebook-messenger-desktop/releases/download/v#{version}/Messenger-#{version}-macos-x64.zip"
   end
 
   name "Messenger"
   desc "Desktop client for Facebook Messenger"
-  homepage "https://github.com/apotenza92/FacebookMessengerDesktop"
+  homepage "https://github.com/apotenza92/facebook-messenger-desktop"
 
   livecheck do
     url :url
