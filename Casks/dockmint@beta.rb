e329072989bc18e3bd897226ab71bab77a63fe70
Alex <apotenza92@gmail.com>
2026-03-12 18:11:19 +1100

    fix: restore beta cask compatibility for v0.2.2

--- Casks/dockmint@beta.rb
+++ Casks/dockmint@beta.rb
@@ -2,34 +2,38 @@ cask "dockmint@beta" do
   version "0.2.2"
 
   on_arm do
-    url "https://github.com/apotenza92/dockmint/releases/download/v0.2.2/Dockmint-v0.2.2-macos-arm64.zip"
-    sha256 "707e37e7117e3907fb0d371fb78b9f474c2c837e2f4de7d5896d8380e0d7b537"
+    url "https://github.com/apotenza92/dockmint/releases/download/v0.2.2/Dockmint-Beta-v0.2.2-macos-arm64.zip"
+    sha256 "d26b026d1d5b00bd923dc5555e4ce431218a452d29bddc9b4a4fa7cb01a7f69f"
   end
 
   on_intel do
-    url "https://github.com/apotenza92/dockmint/releases/download/v0.2.2/Dockmint-v0.2.2-macos-x64.zip"
-    sha256 "90697c5b11c7a849bb3289ff92c99da919e7fabf534af0e0c709983784b01a3e"
+    url "https://github.com/apotenza92/dockmint/releases/download/v0.2.2/Dockmint-Beta-v0.2.2-macos-x64.zip"
+    sha256 "bb71d6c01a8f190d1633c2fe3fbda534f29b1b4c9bd31273ed97316b4433ec7d"
   end
 
-  name "Dockmint"
-  desc "Stable convergence channel for Dockmint beta users"
+  name "Dockmint Beta"
+  desc "Beta channel for Dockmint"
   homepage "https://github.com/apotenza92/dockmint"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://api.github.com/repos/apotenza92/dockmint/releases"
+    strategy :json do |json|
+      json
+        .reject { |release| release["draft"] }
+        .map { |release| release["tag_name"] }
+    end
   end
 
-  app "Dockmint.app"
+  app "Dockmint Beta.app"
 
   zap trash: [
-    "~/Library/Application Support/Dockmint",
-    "~/Library/Application Support/Docktor",
-    "~/Library/Caches/pzc.Dockter",
-    "~/Library/Caches/pzc.Dockmint",
-    "~/Library/Preferences/pzc.Dockter.plist",
-    "~/Library/Preferences/pzc.Dockmint.plist",
-    "~/Library/Saved Application State/pzc.Dockter.savedState",
-    "~/Library/Saved Application State/pzc.Dockmint.savedState",
+    "~/Library/Application Support/Dockmint Beta",
+    "~/Library/Application Support/Docktor Beta",
+    "~/Library/Caches/pzc.Dockter.beta",
+    "~/Library/Caches/pzc.Dockmint.beta",
+    "~/Library/Preferences/pzc.Dockter.beta.plist",
+    "~/Library/Preferences/pzc.Dockmint.beta.plist",
+    "~/Library/Saved Application State/pzc.Dockter.beta.savedState",
+    "~/Library/Saved Application State/pzc.Dockmint.beta.savedState",
   ]
 end
